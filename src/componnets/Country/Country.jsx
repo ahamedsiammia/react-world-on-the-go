5115e949d3fe9ab4333d12555debe0d15d7fd65a
ahamedsiammia <md7559263@gmail.com>
2025-09-19 00:38:44 +0600

    added new fetuare

--- src/componnets/Country/Country.jsx
+++ src/componnets/Country/Country.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import { useState } from 'react';
 import './country.css';
 const Country = ({country}) => {
-    console.log(country)
+
+    const [visited,setVisited] = useState(false);
+    // console.log(country.area.area)
+    const handleVisited = () =>{
+        // * basic system
+        // if(visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+
+        // scoend system
+        // setVisited(visited ? false : true)
+
+        // third system
+        setVisited(!visited)
+    }
+
     return (
-        <div className='country'>
+        // <div className={`country ${visited ? 'country-visited':'not visited'}`}>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} ></img>
             <h3>Name : {country.name.common}</h3>
             <p> population : {country.population.population}</p>
+            <p>Area : {country.area.area} { country.area.area > 300000 ?'Big country' : 'Small country' }</p>
+            <button onClick={handleVisited}>{ visited ? 'visited' : 'Not visited' }</button>
         </div>
     );
 };
